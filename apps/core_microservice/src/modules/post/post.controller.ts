a11b1e9309ec0357fa0113e963e38faaf7a7e147
Advancedboy <legobuilder0819@gmail.com>
2026-02-18 20:13:37 +0300

    chore:fix nested git repo and add all apps

--- apps/core_microservice/src/modules/post/post.controller.ts
+++ apps/core_microservice/src/modules/post/post.controller.ts
@@ -8,16 +8,14 @@ import {
   Delete,
 } from '@nestjs/common';
 import { PostService } from './post.service';
-import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
 
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
   @Post()
-  create(@Body() createPostDto: CreatePostDto) {
-    return this.postService.create(createPostDto);
+  create() {
+    return this.postService.create();
   }
 
   @Get()
@@ -31,8 +29,8 @@ export class PostController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-    return this.postService.update(+id, updatePostDto);
+  update(@Param('id') id: string) {
+    return this.postService.update(+id);
   }
 
   @Delete(':id')
